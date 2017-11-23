f4d4923cfba3a08ffca8e73b564384490b93a759
Thanatermesis <thanatermesis@gmail.com>
2017-11-23 01:35:42 +0100

    DISABLED all the emodule functionality to let to be used only internally

--- src/e_mod_config.c
+++ src/e_mod_config.c
@@ -10,9 +10,9 @@ struct _E_Config_Dialog_Data
 
 static void *_create_data(E_Config_Dialog *cfd);
 static void  _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
-static void _cb_first_setting_change(void *data, Evas_Object *obj);
+/*static void _cb_first_setting_change(void *data, Evas_Object *obj);*/
 static void _fill_data(E_Config_Dialog_Data *cfdata);
-static int _basic_check_changed(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata);
+/*static int _basic_check_changed(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata);*/
 
 static void *
 _create_data(E_Config_Dialog *cfd)
@@ -43,59 +43,60 @@ _basic_apply_data(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
     e_config_save_queue();
     return 1;
 }
-static Evas_Object *
-_basic_create_widgets(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data *cfdata)
-{
-   Evas_Object *o, *ob, *of;
 
-   o = e_widget_list_add(evas, 0, 0);
+/*static Evas_Object **/
+/*_basic_create_widgets(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data *cfdata)*/
+/*{*/
+   /*Evas_Object *o, *ob, *of;*/
 
-   of = e_widget_framelist_add(evas, "General Settings", 0);
-   ob = e_widget_check_add(evas, "First setting to save", &(cfdata->set));
+   /*o = e_widget_list_add(evas, 0, 0);*/
 
-   cfdata->obj = ob;
-   e_widget_on_change_hook_set(ob, _cb_first_setting_change, cfdata);
-   e_widget_framelist_object_append(of, ob);
-   e_widget_list_object_append(o, of, 1, 0, 0.5);
+   /*of = e_widget_framelist_add(evas, "General Settings", 0);*/
+   /*ob = e_widget_check_add(evas, "First setting to save", &(cfdata->set));*/
 
-   e_dialog_resizable_set(cfd->dia, 0);
-   return o;
-}
+   /*cfdata->obj = ob;*/
+   /*e_widget_on_change_hook_set(ob, _cb_first_setting_change, cfdata);*/
+   /*e_widget_framelist_object_append(of, ob);*/
+   /*e_widget_list_object_append(o, of, 1, 0, 0.5);*/
 
-E_Config_Dialog *
-e_int_config_elivehelper_module(E_Container *con, const char *params __UNUSED__)
-{
-    E_Config_Dialog *cfd;
-    E_Config_Dialog_View *v;
+   /*e_dialog_resizable_set(cfd->dia, 0);*/
+   /*return o;*/
+/*}*/
 
-    if(e_config_dialog_find("E", "elive/elivehelper")) return NULL;
-    v = E_NEW(E_Config_Dialog_View, 1);
-    v->create_cfdata = _create_data;
-    v->free_cfdata = _free_data;
-    v->basic.create_widgets = _basic_create_widgets;
-    v->basic.apply_cfdata = _basic_apply_data;
+/*E_Config_Dialog **/
+/*e_int_config_elivehelper_module(E_Container *con, const char *params __UNUSED__)*/
+/*{*/
+    /*E_Config_Dialog *cfd;*/
+    /*E_Config_Dialog_View *v;*/
 
-    cfd = e_config_dialog_new(con, "Elive Helper Settings",
-            "E", "elive/elivehelper",
-            "preferences-engine", 0, v, NULL);
-    e_dialog_resizable_set(cfd->dia, 1);
-    elivehelper_config->cfd = cfd;
+    /*if(e_config_dialog_find("E", "elive/elivehelper")) return NULL;*/
+    /*v = E_NEW(E_Config_Dialog_View, 1);*/
+    /*v->create_cfdata = _create_data;*/
+    /*v->free_cfdata = _free_data;*/
+    /*v->basic.create_widgets = _basic_create_widgets;*/
+    /*v->basic.apply_cfdata = _basic_apply_data;*/
 
-    return cfd;
-}
+    /*cfd = e_config_dialog_new(con, "Elive Helper Settings",*/
+            /*"E", "elive/elivehelper",*/
+            /*"preferences-engine", 0, v, NULL);*/
+    /*e_dialog_resizable_set(cfd->dia, 1);*/
+    /*elivehelper_config->cfd = cfd;*/
 
-static void
-_cb_first_setting_change(void *data, Evas_Object *obj)
-{
-    E_Config_Dialog_Data *cfdata;
-    int val;
+    /*return cfd;*/
+/*}*/
 
-    cfdata = data;
-    val = e_widget_check_checked_get(obj);
+/*static void*/
+/*_cb_first_setting_change(void *data, Evas_Object *obj)*/
+/*{*/
+    /*E_Config_Dialog_Data *cfdata;*/
+    /*int val;*/
 
-    cfdata->set = val;
-    e_widget_check_checked_set(cfdata->obj,val);
-}
+    /*cfdata = data;*/
+    /*val = e_widget_check_checked_get(obj);*/
+
+    /*cfdata->set = val;*/
+    /*e_widget_check_checked_set(cfdata->obj,val);*/
+/*}*/
 
 
 
--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -2,31 +2,31 @@
 #include "e_mod_main.h"
 
 /* gadcon requirements */
-static E_Gadcon_Client *_gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style);
-static void _gc_shutdown(E_Gadcon_Client *gcc);
-static void _gc_orient(E_Gadcon_Client *gcc, E_Gadcon_Orient orient);
-static const char *_gc_label(const E_Gadcon_Client_Class *client_class);
-static Evas_Object *_gc_icon(const E_Gadcon_Client_Class *client_class, Evas *evas);
-static const char *_gc_id_new(const E_Gadcon_Client_Class *client_class);
-static Config_Item *_conf_item_get(const char *id);
+/*static E_Gadcon_Client *_gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style);*/
+/*static void _gc_shutdown(E_Gadcon_Client *gcc);*/
+/*static void _gc_orient(E_Gadcon_Client *gcc, E_Gadcon_Orient orient);*/
+/*static const char *_gc_label(const E_Gadcon_Client_Class *client_class);*/
+/*static Evas_Object *_gc_icon(const E_Gadcon_Client_Class *client_class, Evas *evas);*/
+/*static const char *_gc_id_new(const E_Gadcon_Client_Class *client_class);*/
+/*static Config_Item *_conf_item_get(const char *id);*/
 
 /* and actually define the gadcon class that this module provides (just 1) */
-static const E_Gadcon_Client_Class _gadcon_class =
-{
-   GADCON_CLIENT_CLASS_VERSION,
-   "Elive Helper",
-   {
-      _gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon, _gc_id_new, NULL, NULL
-   },
-   E_GADCON_CLIENT_STYLE_PLAIN
-};
+/*static const E_Gadcon_Client_Class _gadcon_class =*/
+/*{*/
+   /*GADCON_CLIENT_CLASS_VERSION,*/
+   /*"Elive Helper",*/
+   /*{*/
+      /*_gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon, _gc_id_new, NULL, NULL*/
+   /*},*/
+   /*E_GADCON_CLIENT_STYLE_PLAIN*/
+/*};*/
 /**/
 /***************************************************************************/
 
 /***************************************************************************/
-static void _elivehelper_cb_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info);
-static void _elivehelper_menu_cb_post(void *data, E_Menu *m);
-static void _elivehelper_menu_cb_cfg(void *data, E_Menu *menu, E_Menu_Item *mi);
+/*static void _elivehelper_cb_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info);*/
+/*static void _elivehelper_menu_cb_post(void *data, E_Menu *m);*/
+/*static void _elivehelper_menu_cb_cfg(void *data, E_Menu *menu, E_Menu_Item *mi);*/
 /***************************************************************************/
 
 /***************************************************************************/
@@ -52,132 +52,132 @@ Config *elivehelper_config = NULL;
 static E_Config_DD *conf_edd = NULL;
 static E_Config_DD *conf_item_edd = NULL;
 static E_Action *act;
-static Eina_List *elivehelper_instances = NULL;
+/*static Eina_List *elivehelper_instances = NULL;*/
 /*
  * This function is called when you add the Module to a Shelf or Gadgets, it
  * this is where you want to add functions to do things.
  */
-static E_Gadcon_Client *
-_gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style)
-{
-   Evas_Object *o;
-   E_Gadcon_Client *gcc;
-   Instance *inst;
-   char buf[PATH_MAX];
+/*static E_Gadcon_Client **/
+/*_gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style)*/
+/*{*/
+   /*Evas_Object *o;*/
+   /*E_Gadcon_Client *gcc;*/
+   /*Instance *inst;*/
+   /*char buf[PATH_MAX];*/
 
-   inst = E_NEW(Instance, 1);
+   /*inst = E_NEW(Instance, 1);*/
 
-   /*get plugin path*/
-   snprintf(buf, sizeof(buf), "%s/elivehelper.edj",
-           e_module_dir_get(elivehelper_config->module));
+   /*[>get plugin path<]*/
+   /*snprintf(buf, sizeof(buf), "%s/elivehelper.edj",*/
+           /*e_module_dir_get(elivehelper_config->module));*/
 
-   o = edje_object_add(gc->evas);
-   e_theme_edje_object_set(o, "base/theme/modules/elivehelper",
-           "modules/elivehelper/main");
-   edje_object_file_set(o,buf,"modules/elivehelper/main");
-   edje_object_signal_emit(o, "e,state,unfocused", "e");
-   evas_object_show (o);
+   /*o = edje_object_add(gc->evas);*/
+   /*e_theme_edje_object_set(o, "base/theme/modules/elivehelper",*/
+           /*"modules/elivehelper/main");*/
+   /*edje_object_file_set(o,buf,"modules/elivehelper/main");*/
+   /*edje_object_signal_emit(o, "e,state,unfocused", "e");*/
+   /*evas_object_show (o);*/
 
-   gcc = e_gadcon_client_new(gc, name, id, style, o);
-   gcc->data = inst;
+   /*gcc = e_gadcon_client_new(gc, name, id, style, o);*/
+   /*gcc->data = inst;*/
 
-   inst->gcc = gcc;
-   inst->logo = o;
+   /*inst->gcc = gcc;*/
+   /*inst->logo = o;*/
 
-   evas_object_event_callback_add(o, EVAS_CALLBACK_MOUSE_DOWN,
-	 _elivehelper_cb_mouse_down, inst);
+   /*evas_object_event_callback_add(o, EVAS_CALLBACK_MOUSE_DOWN,*/
+	 /*_elivehelper_cb_mouse_down, inst);*/
 
-   elivehelper_config->instances =
-       eina_list_append(elivehelper_config->instances, inst);
+   /*elivehelper_config->instances =*/
+       /*eina_list_append(elivehelper_config->instances, inst);*/
 
-   return gcc;
-}
+   /*return gcc;*/
+/*}*/
 
 /*
  * This function is called when you remove the Module from a Shelf or Gadgets,
  * what this function really does is clean up, it removes everything the module
  * displays
  */
-static void
-_gc_shutdown(E_Gadcon_Client *gcc)
-{
-   Instance *inst;
+/*static void*/
+/*_gc_shutdown(E_Gadcon_Client *gcc)*/
+/*{*/
+   /*Instance *inst;*/
 
-   inst = gcc->data;
-   if (inst->menu)
-     {
-	e_menu_post_deactivate_callback_set(inst->menu, NULL, NULL);
-	e_object_del(E_OBJECT(inst->menu));
-	inst->menu = NULL;
-     }
-   evas_object_del(inst->logo);
-   free(inst);
-}
+   /*inst = gcc->data;*/
+   /*if (inst->menu)*/
+     /*{*/
+	/*e_menu_post_deactivate_callback_set(inst->menu, NULL, NULL);*/
+	/*e_object_del(E_OBJECT(inst->menu));*/
+	/*inst->menu = NULL;*/
+     /*}*/
+   /*evas_object_del(inst->logo);*/
+   /*free(inst);*/
+/*}*/
 
-static void
-_gc_orient(E_Gadcon_Client *gcc, E_Gadcon_Orient orient __UNUSED__)
-{
-    Instance *inst;
-    Evas_Coord mw, mh;
+/*static void*/
+/*_gc_orient(E_Gadcon_Client *gcc, E_Gadcon_Orient orient __UNUSED__)*/
+/*{*/
+    /*Instance *inst;*/
+    /*Evas_Coord mw, mh;*/
 
-    inst = gcc->data;
-    mw = 0, mh = 0;
-    edje_object_size_min_get(inst->logo, &mw, &mh);
-    if ((mw < 1) || (mh < 1))
-        edje_object_size_min_calc(inst->logo, &mw, &mh);
-    if (mw < 4) mw = 4;
-    if (mh < 4) mh = 4;
-    e_gadcon_client_aspect_set(gcc, mw, mh);
-    e_gadcon_client_min_size_set(gcc, mw, mh);
-}
+    /*inst = gcc->data;*/
+    /*mw = 0, mh = 0;*/
+    /*edje_object_size_min_get(inst->logo, &mw, &mh);*/
+    /*if ((mw < 1) || (mh < 1))*/
+        /*edje_object_size_min_calc(inst->logo, &mw, &mh);*/
+    /*if (mw < 4) mw = 4;*/
+    /*if (mh < 4) mh = 4;*/
+    /*e_gadcon_client_aspect_set(gcc, mw, mh);*/
+    /*e_gadcon_client_min_size_set(gcc, mw, mh);*/
+/*}*/
 
 /*
  * This function sets the Gadcon name of the module, not to confuse this with
  * E_Module_Api
  */
-static const char *
-_gc_label(const E_Gadcon_Client_Class *client_class __UNUSED__)
-{
-   return ("Elive Helper");
-}
+/*static const char **/
+/*_gc_label(const E_Gadcon_Client_Class *client_class __UNUSED__)*/
+/*{*/
+   /*return ("Elive Helper");*/
+/*}*/
 
 /*
  * This functions sets the Gadcon icon, the icon you see when you go to add
  * the module to a Shelf or Gadgets.
  */
-static Evas_Object *
-_gc_icon(const E_Gadcon_Client_Class *client_class __UNUSED__, Evas *evas)
-{
-   Evas_Object *o;
-   char buf[PATH_MAX];
+/*static Evas_Object **/
+/*_gc_icon(const E_Gadcon_Client_Class *client_class __UNUSED__, Evas *evas)*/
+/*{*/
+   /*Evas_Object *o;*/
+   /*char buf[PATH_MAX];*/
 
-   o = edje_object_add(evas);
-   snprintf(buf, sizeof(buf), "%s/e-module-elivehelper.edj",
-           e_module_dir_get(elivehelper_config->module));
-   edje_object_file_set(o, buf, "icon");
-   return o;
-}
+   /*o = edje_object_add(evas);*/
+   /*snprintf(buf, sizeof(buf), "%s/e-module-elivehelper.edj",*/
+           /*e_module_dir_get(elivehelper_config->module));*/
+   /*edje_object_file_set(o, buf, "icon");*/
+   /*return o;*/
+/*}*/
 
 /*
  * This function sets the id for the module, so it can be unique from other
  * modules
  */
-static const char *
-_gc_id_new(const E_Gadcon_Client_Class *client_class __UNUSED__)
-{
-    static char buf[PATH_MAX];
+/*static const char **/
+/*_gc_id_new(const E_Gadcon_Client_Class *client_class __UNUSED__)*/
+/*{*/
+    /*static char buf[PATH_MAX];*/
 
-    snprintf(buf, sizeof(buf), "%s.%d", _gadcon_class.name,
-            eina_list_count(elivehelper_config->instances) + 1);
-    return buf;
-}
+    /*snprintf(buf, sizeof(buf), "%s.%d", _gadcon_class.name,*/
+            /*eina_list_count(elivehelper_config->instances) + 1);*/
+    /*return buf;*/
+/*}*/
 
 static Config_Item *
 _conf_item_get(const char *id)
 {
     Config_Item *ci;
 
-    GADCON_CLIENT_CONFIG_GET(Config_Item, elivehelper_config->items, _gadcon_class, id);
+    /*GADCON_CLIENT_CONFIG_GET(Config_Item, elivehelper_config->items, _gadcon_class, id);*/
 
     ci = E_NEW(Config_Item, 1);
     ci->id = eina_stringshare_add(id);
@@ -188,93 +188,93 @@ _conf_item_get(const char *id)
     return ci;
 }
 
-static void
-_elivehelper_menu_cb_cfg(void *data, E_Menu *menu __UNUSED__, E_Menu_Item *mi __UNUSED__)
-{
-    Instance *inst = data;
-    E_Container *con;
+/*static void*/
+/*_elivehelper_menu_cb_cfg(void *data, E_Menu *menu __UNUSED__, E_Menu_Item *mi __UNUSED__)*/
+/*{*/
+    /*Instance *inst = data;*/
+    /*E_Container *con;*/
 
-    if(inst->popup)
-    {
-        e_object_del(E_OBJECT(inst->popup));
-        inst->popup = NULL;
-    }
-    con = e_container_current_get(e_manager_current_get());
-    e_int_config_elivehelper_module(con, NULL);
-}
+    /*if(inst->popup)*/
+    /*{*/
+        /*e_object_del(E_OBJECT(inst->popup));*/
+        /*inst->popup = NULL;*/
+    /*}*/
+    /*con = e_container_current_get(e_manager_current_get());*/
+    /*e_int_config_elivehelper_module(con, NULL);*/
+/*}*/
 
-static void
-_elivehelper_cb_mouse_down(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info)
-{
-    Instance *inst;
-    Evas_Event_Mouse_Down *ev;
+/*static void*/
+/*_elivehelper_cb_mouse_down(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info)*/
+/*{*/
+    /*Instance *inst;*/
+    /*Evas_Event_Mouse_Down *ev;*/
 
-    inst = data;
-    ev = event_info;
-    if((ev->button == 1) && !(inst->menu))
-    {
-	    int x, y;
+    /*inst = data;*/
+    /*ev = event_info;*/
+    /*if((ev->button == 1) && !(inst->menu))*/
+    /*{*/
+	    /*int x, y;*/
 
-        inst->menu = e_int_menus_main_new();
+        /*inst->menu = e_int_menus_main_new();*/
 
-        e_menu_post_deactivate_callback_set(inst->menu,
-                _elivehelper_menu_cb_post, inst);
+        /*e_menu_post_deactivate_callback_set(inst->menu,*/
+                /*_elivehelper_menu_cb_post, inst);*/
 
-        e_gadcon_locked_set(inst->gcc->gadcon,1);
-        e_gadcon_canvas_zone_geometry_get(inst->gcc->gadcon, &x, &y, NULL, NULL);
+        /*e_gadcon_locked_set(inst->gcc->gadcon,1);*/
+        /*e_gadcon_canvas_zone_geometry_get(inst->gcc->gadcon, &x, &y, NULL, NULL);*/
 
-        e_menu_activate_mouse(inst->menu,
-                e_util_zone_current_get(e_manager_current_get())
-                , x + ev->output.x, y + ev->output.y,
-                1, 1, E_MENU_POP_DIRECTION_AUTO, ev->timestamp);
+        /*e_menu_activate_mouse(inst->menu,*/
+                /*e_util_zone_current_get(e_manager_current_get())*/
+                /*, x + ev->output.x, y + ev->output.y,*/
+                /*1, 1, E_MENU_POP_DIRECTION_AUTO, ev->timestamp);*/
 
-        edje_object_signal_emit(inst->logo, "e,state,focused", "e");
-    }
-    else if ((ev->button == 3) && (!inst->menu))
-    {
-        E_Zone *zone;
-        E_Menu *m;
-        E_Menu_Item *mi;
-        int x, y;
+        /*edje_object_signal_emit(inst->logo, "e,state,focused", "e");*/
+    /*}*/
+    /*else if ((ev->button == 3) && (!inst->menu))*/
+    /*{*/
+        /*E_Zone *zone;*/
+        /*E_Menu *m;*/
+        /*E_Menu_Item *mi;*/
+        /*int x, y;*/
 
-        zone = e_util_zone_current_get(e_manager_current_get());
+        /*zone = e_util_zone_current_get(e_manager_current_get());*/
 
-        m = e_menu_new();
-        mi = e_menu_item_new(m);
-        e_menu_item_label_set(mi, "Settings");
-        e_util_menu_item_theme_icon_set(mi, "configure");
-        e_menu_item_callback_set(mi, _elivehelper_menu_cb_cfg, inst);
-        m = e_gadcon_client_util_menu_items_append(inst->gcc, m, 0);
-        e_menu_post_deactivate_callback_set(m, _elivehelper_menu_cb_post, inst);
-        inst->menu = m;
+        /*m = e_menu_new();*/
+        /*mi = e_menu_item_new(m);*/
+        /*e_menu_item_label_set(mi, "Settings");*/
+        /*e_util_menu_item_theme_icon_set(mi, "configure");*/
+        /*e_menu_item_callback_set(mi, _elivehelper_menu_cb_cfg, inst);*/
+        /*m = e_gadcon_client_util_menu_items_append(inst->gcc, m, 0);*/
+        /*e_menu_post_deactivate_callback_set(m, _elivehelper_menu_cb_post, inst);*/
+        /*inst->menu = m;*/
 
-        e_gadcon_canvas_zone_geometry_get(inst->gcc->gadcon, &x, &y, NULL, NULL);
-        e_menu_activate_mouse(m, zone, x + ev->output.x, y + ev->output.y,
-                              1, 1, E_MENU_POP_DIRECTION_AUTO, ev->timestamp);
+        /*e_gadcon_canvas_zone_geometry_get(inst->gcc->gadcon, &x, &y, NULL, NULL);*/
+        /*e_menu_activate_mouse(m, zone, x + ev->output.x, y + ev->output.y,*/
+                              /*1, 1, E_MENU_POP_DIRECTION_AUTO, ev->timestamp);*/
 
-        evas_event_feed_mouse_up(inst->gcc->gadcon->evas, ev->button,
-                                 EVAS_BUTTON_NONE, ev->timestamp, NULL);
-     }
-}
+        /*evas_event_feed_mouse_up(inst->gcc->gadcon->evas, ev->button,*/
+                                 /*EVAS_BUTTON_NONE, ev->timestamp, NULL);*/
+     /*}*/
+/*}*/
 
 /*
  * This function is a call-back it removes the menu after the use clicks on
  * a item in the menu or click of the menu, basically it removes the menu
  * from being displayed.
  */
-static void
-_elivehelper_menu_cb_post(void *data, E_Menu *m __UNUSED__)
-{
-    Instance *inst;
+/*static void*/
+/*_elivehelper_menu_cb_post(void *data, E_Menu *m __UNUSED__)*/
+/*{*/
+    /*Instance *inst;*/
 
-    inst = data;
-    if(inst->menu)
-    {
-        edje_object_signal_emit(inst->logo, "e,state,unfocused", "e");
-        e_object_del(E_OBJECT(inst->menu));
-        inst->menu = NULL;
-    }
-}
+    /*inst = data;*/
+    /*if(inst->menu)*/
+    /*{*/
+        /*edje_object_signal_emit(inst->logo, "e,state,unfocused", "e");*/
+        /*e_object_del(E_OBJECT(inst->menu));*/
+        /*inst->menu = NULL;*/
+    /*}*/
+/*}*/
 
 /* This is needed to advertise a label for the module IN the code (not just
  * the .desktop file) but more specifically the api version it was compiled
@@ -299,7 +299,7 @@ e_modapi_init(E_Module *m)
 #define D conf_item_edd
     E_CONFIG_VAL(D, T, id, STR);
 
-    conf_edd = E_CONFIG_DD_NEW("Elive Helper_Config", Config);
+    conf_edd = E_CONFIG_DD_NEW("Elive_Helper_Config", Config);
 #undef T
 #undef D
 #define T Config
@@ -312,7 +312,7 @@ e_modapi_init(E_Module *m)
         elivehelper_config = E_NEW(Config, 1);
 
     elivehelper_config->module = m;
-    e_gadcon_provider_register(&_gadcon_class);
+    /*e_gadcon_provider_register(&_gadcon_class);*/
     return m;
 }
 
@@ -325,7 +325,7 @@ e_modapi_shutdown(E_Module *m __UNUSED__)
 {
     Config_Item *ci;
 
-    e_gadcon_provider_unregister(&_gadcon_class);
+    /*e_gadcon_provider_unregister(&_gadcon_class);*/
 
     if(elivehelper_config)
     {
